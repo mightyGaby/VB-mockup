3585eaa9170c8710cb7129f5e6694fc0887ccd5e
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2016-01-11 19:57:57 -0600

    blah

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('.carousel-slider').carousel();
+
+    // $('.materialboxed').materialbox();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
