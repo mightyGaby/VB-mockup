cbf9249328ce013d439f53f1319fa81d4d11a8df
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2016-01-11 23:00:59 -0600

    formats video popup and js

--- js/init.js
+++ js/init.js
@@ -1,5 +1,52 @@
+var video = $(".responsive-video")[0]; // id or class of your <video> tag
+
+function showVideo(){
+  $('.fa-video-camera').click(function(){
+    $('#highlight-reel').removeClass('hide');
+    $('.responsive-video')
+      .addClass('vid-init');
+    $('#index-banner').addClass('dark-overlay');
+      if (video.paused) {
+          video.play();
+      }
+  })
+
+  // $(document).click(function(e){
+  //   console.log(e.target);
+  //     if( $(e.target).is($('.responsive-video')) ){
+  //       console.log('true');
+  //       $('#highlight-reel').addClass('hide');
+  //       $('.responsive-video').removeClass('vid-init');
+  //       $('#index-banner').removeClass('dark-overlay');
+  //     }
+  // })
+}
+
+function hideVideo(){
+  $(document).mouseup(function (e)
+{
+    var container = $(".responsive-video");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        // container.hide();
+              $('#highlight-reel').addClass('hide');
+              $('.responsive-video').removeClass('vid-init');
+              $('#index-banner').removeClass('dark-overlay');
+              if (!video.paused) {
+                  video.pause();
+              }
+    }
+});
+}
+
 (function($){
   $(function(){
+    showVideo();
+    hideVideo();
+
+    $('.modal-trigger').leanModal();
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
