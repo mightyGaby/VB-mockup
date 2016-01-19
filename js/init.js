18f5fcaca10b849ba7ae81ffc57adc84e3f4242b
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2016-01-18 22:28:43 -0600

    adds scroll

--- js/init.js
+++ js/init.js
@@ -77,6 +77,17 @@ function hideVideo(){
 
 (function($){
 
+  $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop':  $target.offset().top //no need of parseInt here
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
 
   $('#textarea1').val(' ');
   $('#textarea1').trigger('autoresize');
